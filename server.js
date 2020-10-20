77b9abf39fd364a6c7b55c8dd1d3a4de7fbfb9cf
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-20 12:09:19 -0700

    testing travis

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ require("./routes/api-routes.js")(app);
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
+    console.log("hello!");
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
