92ae6eaed827d62d89d7052aa3bfdbe76a092bf5
git99.src <cpu.al2@gmail.com>
2020-10-21 11:13:25 -0700

    Added dotenv and handling to project

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // Requiring necessary npm packages
+require('dotenv').config()
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -15,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: process.env.SERVER_SECRET, resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
