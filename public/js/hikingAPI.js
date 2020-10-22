fe4d9bcef395d1dab7128f0c932a24d5842a4c2a
Marylee Pabst <marylee@uconn.edu>
2020-10-22 11:55:33 -0700

    html added

--- public/js/hikingAPI.js
+++ public/js/hikingAPI.js
@@ -14,7 +14,7 @@ function hikeQuery(hikeBaseURL) {
         console.log("hikeData:", hikeData);
         console.log("QueryURL:", hikeBaseURL);
         console.log("===========================");
-
+        //curent trail data
         console.log("trail", hikeData.trails.name);
         console.log("Summary", hikeData.trails.summary);
         console.log("difficulty", hikeData.trails.difficulty);
@@ -35,6 +35,27 @@ function hikeQuery(hikeBaseURL) {
         console.log("conditionDetails", hikeData.trails.conditionDetails);
         console.log("conditionDate", hikeData.trails.conditionDate);
 
+        //dump info to HTML
+        let trailName = $('<h2>').text(hikeData.trails.name + " " + hikeData.list[0].dt_txt);
+        let trailLocation = $('<h3>').text(hikeData.list[0].main.location);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDifficulty = $('<p>').text("Difficulty: " + hikeData.list[0].main.difficulty);
+        let trailRating = $('<p>').text("Rating: " + hikeData.list[0].main.stars);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailDescription = $('<p>').text("Summary: " + hikeData.list[0].main.summary);
+        let trailLink = $('<p>').text("Learn more: " + hikeData.list[0].main.url);
+
+
+
+
+
+        // longitude and latitude variables for url
         let longitude = "&lon=" + hikeData.trails.longitude;
         console.log(longitude);
         let latitude = "lat=" + hikeData.trails.latitude;
