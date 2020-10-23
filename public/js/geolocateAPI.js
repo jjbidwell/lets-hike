814209e50fc9e090795008cc2895627a8b03756f
Joshua Bidwell <jjbidwell262@gmail.com>
2020-10-23 11:58:03 -0700

    got api logging data

--- public/js/geolocateAPI.js
+++ public/js/geolocateAPI.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const geolocate = searchLocation => {
+const apiCall = (searchLocation, minLength, maxLength, maxAscent) => {
   const key = "iqdeIphOmFTHdvGRonpZrdKkjACvb5Sg";
   const loc = searchLocation;
   const queryUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${loc}`;
@@ -11,7 +11,26 @@ const geolocate = searchLocation => {
     const long = coords.lng;
     console.log("Latitude: " + lat);
     console.log("Longitude: " + long);
+    hikeQuery(lat, long, minLength, maxLength, maxAscent);
   });
 };
 
-module.exports = geolocate;
+function hikeQuery(lat, long, minLength, maxLength, maxAscent) {
+  const hikeApiKey = "200954275-61d35dbb141f7d0585437ea6275153f0";
+  const hikeBaseUrl = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&minLength=${minLength}&maxDistance=25&maxResults=50&key=${hikeApiKey}`;
+  //console.log(hikingUrl);
+  axios.get(hikeBaseUrl).then(response => {
+    //console.log(maxAscent);
+    const trailList = response.data.trails;
+    trailList.forEach(element => {
+      if (element.length <= maxLength && element.ascent <= maxAscent) {
+        console.log("Name: " + element.name);
+        console.log("Length: " + element.length);
+        console.log("Ascent: " + element.ascent);
+        console.log("URL: " + element.url);
+      }
+    });
+  });
+}
+
+module.exports = apiCall;
